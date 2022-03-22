8fa168e84e27170c38833fe19e02d6c63af45341
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-22 10:48:48 +0400

    updating in moving of parts

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 # Day 20
 
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
+screen.tracer(0)
 snake_parts = []
 
+for i in range(0, 60, 20):
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.penup()
+    snake.setposition(-1 * i, 0)
+    snake.speed("slow")
+    snake_parts.append(snake)
 
-def start():
-    for i in range(0, 60, 20):
-        snake = Turtle(shape="square")
-        snake.color("white")
-        snake.setposition(-1 * i, 0)
-        snake_parts.append(snake)
-
-
-def move():
-    while True:
-        for snake in snake_parts:
-            previous_position = snake.position()[0]
-            snake.goto(previous_position + 20, 0)
-
-
-start()
-move()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for part in range(len(snake_parts) - 1, 0, -1):
+        new_x = snake_parts[part - 1].xcor()
+        new_y = snake_parts[part - 1].ycor()
+        snake_parts[part].goto(new_x, new_y)
+    snake_parts[0].forward(20)
 
 screen.exitonclick()
