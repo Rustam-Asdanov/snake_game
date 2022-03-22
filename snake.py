71b56a3ebb64655fcefc25858d80189abfe56e20
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-22 11:17:19 +0400

    add key functions for moving the snake

--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 class Snake:
 
@@ -8,11 +10,11 @@ class Snake:
         self.create_snake()
 
     def create_snake(self):
-        for i in range(0, 60, 20):
+        for position in STARTING_POSITIONS:
             snake = Turtle(shape="square")
             snake.color("white")
             snake.penup()
-            snake.setposition(-1 * i, 0)
+            snake.goto(position)
             snake.speed("slow")
             self.snake_parts.append(snake)
 
@@ -21,4 +23,16 @@ class Snake:
             new_x = self.snake_parts[part - 1].xcor()
             new_y = self.snake_parts[part - 1].ycor()
             self.snake_parts[part].goto(new_x, new_y)
-        self.snake_parts[0].forward(20)
+        self.snake_parts[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.snake_parts[0].setheading(90)
+
+    def down(self):
+        self.snake_parts[0].setheading(270)
+
+    def left(self):
+        self.snake_parts[0].setheading(180)
+
+    def right(self):
+        self.snake_parts[0].setheading(0)
